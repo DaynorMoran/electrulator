162be2f2f27b3df857629f954ad6b5ed3c2513fd
Daynor Moran <gabriel.daynor.moran.valencia@outlook.com>
2024-08-11 19:58:23 -0400

    primer

---
+++ forms/form_bases.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Electrulator - Bases</title>
+    <!-- Añadir bootstrap 5 ↓ -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/app.css">
+
+</head>
+<body>
+
+    <!-- agrego boootstrap 5 JS↓↓↓ -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Electrulator</title>
+    <!-- Añadir bootstrap 5 ↓ -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+
+<body class="bg-primary d-flex flex-column vh-100">
+
+    <header class="bg-primary  sticky-top">
+        <div class="container text-center">
+            <h1 class="text-accent fuente-Bebas display-4">
+                Electrulat<span>
+                    <img src="assets/img/foco.png" alt="O" style="height: 0.7em; vertical-align: middle;">
+                </span>r
+            </h1>
+        </div>
+    </header>
+
+    <!-- fin header -->
+
+
+    <div class="container py-2">
+        <div class="row justify-content-center mt-2">
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card h-100">
+                    <img src="assets/img/resistencia.png" class="w-70 card-img-top img-fluid p-4 card-img-custom"
+                        alt="Imagen de resistencia">
+                    <div class="card-body">
+                        <h2 class="fuente-Bebas text-secondary">Calculadora de código de colores de resistencias</h2>
+                        <p class="card-text fuente-Abel">Una útil herramienta para identificar el valor de una
+                            resistencia mediante
+                            el código de colores.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- fin card 1 -->
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card h-100">
+                    <img src="assets/img/condensador.png" class="w-70 card-img-top img-fluid p-4 card-img-custom"
+                        alt="Imagen de condensador">
+                    <div class="card-body">
+                        <h2 class="fuente-Bebas text-secondary">Calculadora de código de condensadores</h2>
+                        <p class="card-text fuente-Abel">Una útil herramienta para calcular el valor de un condensador a
+                            partir de
+                            su código numérico.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- fin card 2 -->
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card h-100">
+                    <img src="assets/img/binario.png" class="w-75 card-img-top img-fluid p-4 card-img-custom"
+                        alt="Imagen de calculadora binaria">
+                    <div class="card-body">
+                        <h2 class="fuente-Bebas text-secondary">Calculadora de bases numéricas</h2>
+                        <p class="card-text fuente-Abel">Herramienta versátil para convertir números decimales a
+                            binario, octal y
+                            hexadecimal.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- fin card 3 -->
+        </div>
+    </div>
+
+
+    <footer class="bg-secondary text-white py-2 mt-5 mt-auto">
+        <div class="container text-center">
+            <p class="fuente-Abel small">2024</p>
+            <div class="container">
+                <div class="row flex-column-reverse flex-md-row align-items-center">
+                    <div class="col-12 col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        <p class="small">"❤️La educación es el arma más poderosa que puedes usar para cambiar el mundo."
+                            - Nelson Mandela</p>
+                    </div>
+                    <div class="col-12 col-md-6 text-center text-md-end mb-3 mb-md-0">
+                        <button class="btn btn-dark">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M12 0.296997C5.37 0.296997 0 5.67 0 12.297C0 17.6 3.438 22.097 8.205 23.682C8.805 23.795 9.025 23.424 9.025 23.105C9.025 22.82 9.015 22.065 9.01 21.065C5.672 21.789 4.968 19.455 4.968 19.455C4.422 18.07 3.633 17.7 3.633 17.7C2.546 16.956 3.717 16.971 3.717 16.971C4.922 17.055 5.555 18.207 5.555 18.207C6.625 20.042 8.364 19.512 9.05 19.205C9.158 18.429 9.467 17.9 9.81 17.6C7.145 17.3 4.344 16.268 4.344 11.67C4.344 10.36 4.809 9.29 5.579 8.45C5.444 8.147 5.039 6.927 5.684 5.274C5.684 5.274 6.689 4.952 8.984 6.504C9.944 6.237 10.964 6.105 11.984 6.099C13.004 6.105 14.024 6.237 14.984 6.504C17.264 4.952 18.269 5.274 18.269 5.274C18.914 6.927 18.509 8.147 18.389 8.45C19.154 9.29 19.619 10.36 19.619 11.67C19.619 16.28 16.814 17.295 14.144 17.59C14.564 17.95 14.954 18.686 14.954 19.81C14.954 21.416 14.939 22.706 14.939 23.096C14.939 23.411 15.149 23.786 15.764 23.666C20.565 22.092 24 17.592 24 12.297C24 5.67 18.627 0.296997 12 0.296997Z"
+                                    fill="white"></path>
+                            </svg>
+                            <span class="ms-2">Visitanos en GitHub</span>
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+    </footer>
+    <!-- agrego boootstrap 5 JS↓↓↓ -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
